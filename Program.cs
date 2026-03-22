bf356b322879d01643af1e9a6f87030b14b5e5dd
Bartłomiej Iskrzycki <bartek@brokul.dev>
2026-03-22 12:43:02 +0100

    demo adjusted 2

--- Program.cs
+++ Program.cs
@@ -9,16 +9,16 @@ builder.Configuration.AddBlobJson(
     container: "myconfig",
     blobName: "config.json",
     credential: new AzureCliCredential(),
-    loggerFactory: app.Services.GetRequiredService<ILoggerFactory>());  
+    loggerFactory: app.Services.GetRequiredService<ILoggerFactory>());
 
 app.MapGet("/", (IConfiguration configuration) =>
 {
     var configSection = configuration.GetSection("BlobConfig");
 
-    var config1 = configSection["Config1"];
-    var config2 = configSection["Config2"];
+    var config1 = configSection["config1"];
+    var config2 = configSection["config2"];
 
-    return $"Config1: {config1}, Config2: {config2}";
+    return $"config1: {config1}, config2: {config2}";
 });
 
 app.Run();
