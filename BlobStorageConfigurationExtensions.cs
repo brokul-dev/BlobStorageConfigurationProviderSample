ab5d7d3672c499dba0654d1b0cb40df8be79621d
Bartłomiej Iskrzycki <bartek@brokul.dev>
2026-03-22 16:31:07 +0100

    extension method adjustment

--- BlobStorageConfigurationExtensions.cs
+++ BlobStorageConfigurationExtensions.cs
@@ -4,9 +4,15 @@ namespace BlobStorageConfigurationProviderSample;
 
 public static class BlobStorageConfigurationExtensions
 {
-    public static IConfigurationBuilder AddBlobJson(
-        this IConfigurationBuilder builder, string account, string container, string blobName, 
-        TokenCredential credential, ILoggerFactory loggerFactory) => 
-            builder.Add(new BlobStorageConfigurationSource(
-                account, container, blobName, credential, loggerFactory));
+    extension(IConfigurationBuilder builder)
+    {
+        public IConfigurationBuilder AddBlobJson(
+            string account,
+            string container,
+            string blobName,
+            TokenCredential credential,
+            ILoggerFactory loggerFactory) =>
+                builder.Add(new BlobStorageConfigurationSource(
+                    account, container, blobName, credential, loggerFactory));
+    }
 }
